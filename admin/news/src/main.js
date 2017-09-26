bf85a7178abf0486a329559a7a4df15d701312ff
wyj <ismrwang@gmail.com>
2017-09-26 10:12:33 +0800

    x增加开发环境路由配置

--- admin/news/src/main.js
+++ admin/news/src/main.js
@@ -23,21 +23,25 @@ router.beforeEach((to, from, next) => {
     // 获取access_token
     let isLogin = sessionStorage.getItem('IS_LOGIN');
     // 判断是否已登录，未登录则跳转至登录页
-    if (!isLogin) {
-        window.location.href = '/admin/';
-    }else{
-        // 若已登录，则检查是否已选择公众号
-        let wechatId = sessionStorage.getItem('WECHAT_ID');
-        if (!wechatId && to.path !== '/') {
-            // 错误提示
-            Message.error({
-                message : '请先选择公众号',
-                showClose : true,
-            });
+    if (process.env.NODE_ENV !== 'production') {
+        if (!isLogin) {
             window.location.href = '/admin/';
-        }else{
-            next();
+        } else {
+            // 若已登录，则检查是否已选择公众号
+            let wechatId = sessionStorage.getItem('WECHAT_ID');
+            if (!wechatId && to.path !== '/') {
+                // 错误提示
+                Message.error({
+                    message: '请先选择公众号',
+                    showClose: true,
+                });
+                window.location.href = '/admin/';
+            } else {
+                next();
+            }
         }
+    } else {
+        next();
     }
 });
 
